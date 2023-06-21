286eca0ec700bc77d883e4f82e163857efb454aa
roeygross <roeygross16@gmail.com>
2023-06-21 20:40:38 +0300

    added: chairs tables walls changed: colors sizes positions

--- unittests/buildRoom.java
+++ unittests/buildRoom.java
@@ -16,14 +16,16 @@ public class buildRoom {
     Scene scene = new Scene("buildRoomimage");
     Color SKY = new Color(115,235,249);
     Color LIGHTGRAY = new Color(java.awt.Color.LIGHT_GRAY);
+    Color WHITE = new Color(java.awt.Color.WHITE);
+    Color BLACK = new Color(java.awt.Color.BLACK);
 
     Color RED = new Color(255,0,0);
     Color GRAY = new Color(java.awt.Color.gray);
-
+    Color DARKBLUE = new Color(0, 0, 102);
     Color PURPALE = new Color(67,2,125);
     Color phooksia = new Color(244,108,255);
     Color GREEN = new Color(0,255,0);
-    Color YELLOW = new Color(0,255,255);
+    Color YELLOW = new Color(255,255,0);
     double kt = 0.15;
     double kd = 0.3;
     double ks = 0.3;
@@ -39,7 +41,7 @@ public class buildRoom {
         scene.lights.add(
                 new DirectionalLight(new Color(100,100,100),new Vector(-1.012534429138574, -3.682651905023338, -2))
         );
-        Point cameraPosition = new Point(-900, 500, 200);
+        Point cameraPosition = new Point(-1800, 1000, 400);
 
         Camera camera = new Camera(cameraPosition, new Point(0,0,-1)).switchUpRight() //
                 .setVPSize(200, 200).setVPDistance(100).setAngle(10,new Vector(0,-1,0));
@@ -68,25 +70,101 @@ public class buildRoom {
 
         );*/
         Color WOODCOLOR = new Color(133, 94, 66);
+        Color DARKWOODCOLOR = new Color(77, 38, 0);
         Material WOODMETIRIAL = new Material().setKs(0.1).setKd(0.1).setnShininess(1);
         scene.geometries.add(
+                //walls
                 new Floor (new Point(0, 0, 0), new Vector(0, 0, 1), new Vector(-1, 0, 0),
-                        PURPALE, SKY, 500, 1000, 10, 10,new Material()).getElements()
-    ,new Floor (new Point(0, 0, 0), new Vector(0, 0, 1), new Vector(0, 1, 0),
-                       PURPALE, SKY, 500, 1000, 10, 10,new Material()).getElements()
+                        RED, YELLOW, 2000, 2000, 20, 20,new Material()).getElements()
+                ,new Floor (new Point(0, 0, 0), new Vector(0, 0, 1), new Vector(0, 1, 0),
+                       RED, YELLOW, 2000, 2000, 20, 20,new Material()).getElements()
+                //floor
                 ,new Floor (new Point(0, 0, 0), new Vector(-1, 0, 0), new Vector(0, 1, 0),
-                        YELLOW, LIGHTGRAY, 1000, 1000, 10, 10,new Material().setkR(0.01).setKs(0.1).setKd(0.1).setnShininess(2)).getElements()
+                        BLACK, WHITE, 2000, 2000, 15, 15,new Material().setkR(0.01).setKs(0.1).setKd(0.1).setnShininess(2)).getElements()
+                //roof
                 ,new Floor (new Point(0, 0, 500), new Vector(-1, 0, 0), new Vector(0, 1, 0),
-                        GRAY, GRAY, 1000, 1000, 10, 10,new Material().setkR(0.01).setKs(0.1).setKd(0.1).setnShininess(2)).getElements()
-                ,new Table(0.5,0.25, WOODCOLOR,new Point(-0.5,0.5,0),new Vector(0,0,1),new Vector(0,0,1)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
-                ,new Table(0.5,0.25, WOODCOLOR,new Point(-1,0.5,0),new Vector(0,0,1),new Vector(0,0,1)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
-                ,new Table(0.5,0.25, WOODCOLOR,new Point(-1.5,0.5,0),new Vector(0,0,1),new Vector(0,0,1)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
-                ,new Chair(new Point(-1.5,1.45,1),0.2,0.4,0.005,0.03,0.01,0.01
+                        DARKBLUE, PURPALE, 2000, 2000, 10, 10,new Material().setkR(0.01).setKs(0.1).setKd(0.1).setnShininess(2)).getElements()
+
+                //0,0 is A0, positive y is 1,2,3 and negative x is A B C D
+                //A1,2 Tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-100,600,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-100,200,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-100,700,100),100,200,2.5,15,5,5
                         ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
-                ,new Chair(new Point(-1.1,1.45,1),0.2,0.4,0.005,0.03,0.01,0.01
+                ,new Chair(new Point(-100,500,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-100,300,100),100,200,2.5,15,5,5
+                    ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-100,100,100),100,200,2.5,15,5,5
+                    ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                //A3,4 Tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-600,600,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-600,200,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-600,700,100),100,200,2.5,15,5,5
                         ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
-                ,new Floor(new Point(-0.1,0.1,0.2),new Vector(-1,0,0),new Vector(0,0,1),Color.BLACK,Color.BLACK,3,1,1,1,new Material().setkR(0.5).setKs(0.3).setKd(0.3)).getElements()
-                ,new Floor(new Point(-0.1,0.1,0.2),new Vector(0,1,0),new Vector(0,0,1),Color.BLACK,Color.BLACK,3,1,1,1,new Material().setkR(0.5).setKs(0.3).setKd(0.3)).getElements()
+                ,new Chair(new Point(-600,500,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-600,300,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-600,100,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                //B1,2 Tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-100,1400,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-100,1000,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-100,1500,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-100,1300,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-100,1100,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-100,900,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                //B3, 4 Tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-600,1400,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-600,1000,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-600,1500,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-600,1300,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-600,1100,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-600,900,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                //c1,2 tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-1200,900,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-1200,300,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-1200,1000,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1200,800,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1200,400,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1200,200,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                //d1,2 Tables and chairs
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-1500,900,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Table(160,60, DARKWOODCOLOR,new Point(-1500,300,0),new Vector(0,0,1),new Vector(1,0,0)).setMaterialBars(WOODMETIRIAL).setMaterialBase(WOODMETIRIAL).setMaterialLeg(WOODMETIRIAL).setMaterialSurfaceBase(WOODMETIRIAL).setMaterialSurfaceTop(WOODMETIRIAL).getElements()
+                ,new Chair(new Point(-1500,1000,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1500,800,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1500,400,100),100,200,2.5,15,5,5
+                        ,new Vector(0,-1,0),new Vector(1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+                ,new Chair(new Point(-1500,200,100),100,200,2.5,15,5,5
+                        ,new Vector(0,1,0),new Vector(-1,0,0),WOODCOLOR,true).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).setSeatMaterial(WOODMETIRIAL).setSeatCoverMaterial(WOODMETIRIAL).setBrCoverMaterial(WOODMETIRIAL).setLegsMaterial(WOODMETIRIAL).setBarsMaterial(WOODMETIRIAL).getGeometries()
+
+                //mirrors
+                ,new Floor(new Point(-0.1,0.1,100),new Vector(-1,0,0),new Vector(0,0,1),Color.BLACK,Color.BLACK,2000,200,1,1,new Material().setkR(0.5).setKs(0.3).setKd(0.3)).getElements()
+                ,new Floor(new Point(-0.1,0.1,100),new Vector(0,1,0),new Vector(0,0,1),Color.BLACK,Color.BLACK,2000,200,1,1,new Material().setkR(0.5).setKs(0.3).setKd(0.3)).getElements()
+               //little wall in middle
+                ,new Floor (new Point(-900, 0, 0), new Vector(0, 0, 1), new Vector(0, 1, 0),
+                     GRAY   , LIGHTGRAY, 125, 1200, 15, 15,new Material()).getElements()
+                //walls behind camera
+                ,new Floor (new Point(-1900, 0, 0), new Vector(0, 0, 1), new Vector(0, 1, 0),
+                        RED, YELLOW, 2000, 2000, 20, 20,new Material()).getElements()
+                , new Floor (new Point(0, 2000, 0), new Vector(0, 0, 1), new Vector(-1, 0, 0),
+                        RED, YELLOW, 2000, 2000, 20, 20,new Material()).getElements()
+
 
         );
         scene.lights.add(
@@ -94,7 +172,7 @@ public class buildRoom {
         );
         camera.setAperture(0.1);
 
-        camera.setImageWriter(new ImageWriter("roomImage", 100, 100)) //
+        camera.setImageWriter(new ImageWriter("roomImage", 500, 500)) //
                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage() //
                 .writeToImage();
